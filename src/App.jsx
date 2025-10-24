39d6cd22d8473d3354ab0c31266b77ac29eebf04
Your Name <your.email@example.com>
2025-10-24 15:51:52 +0900

    testsupa

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,30 @@
 import React from 'react';
 
-function App() {
-  return <div>App</div>;
-}
+import { useState, useEffect } from 'react';
+import supabase from './utils/supabase';
 
+function App() {
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    async function getPosts() {
+      const { data: posts } = await supabase.from('posts').select();
+
+      console.log(posts);
+      setPosts(posts);
+    }
+
+    getPosts();
+  }, []);
+
+  return (
+    <div>
+      {posts.map((post) => (
+        <li key={post.id}>
+          {post.id} / {post.title}/{post.name}/{post.content}
+        </li>
+      ))}
+    </div>
+  );
+}
 export default App;
