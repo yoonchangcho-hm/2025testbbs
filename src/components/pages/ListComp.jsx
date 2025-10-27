a06a0a4bb0664d1d9b336f1475830b3a9c7f6c1b
Your Name <your.email@example.com>
2025-10-27 14:18:21 +0900

    251027_1420

--- src/components/pages/ListComp.jsx
+++ src/components/pages/ListComp.jsx
@@ -1,7 +1,19 @@
 import React from 'react';
 
 function ListComp() {
-  return <div>ListComp</div>;
+  return (
+    <div>
+      <h3>post</h3>
+      <ul>
+        {posts.map((post) => (
+          <li key={post.id}>
+            {post.title} / {post.name} / {post.content}/{' '}
+            {dayjs(post.created_at).format('YYYY-MM-DD')}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
 
 export default ListComp;
